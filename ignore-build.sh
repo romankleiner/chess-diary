7af500ec5cbb90a4c74b1be1e592d9686dbdb342
romankleiner <romank66@yahoo.com>
2026-02-12 12:13:17 +0000

    Update ignore-build.sh

--- ignore-build.sh
+++ ignore-build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Skip build if ONLY todo.txt changed
-if git diff --name-only HEAD^ HEAD | grep -q "^todo.txt$"; then
+if git diff --name-only HEAD^ HEAD | grep -q "^todoList.txt$"; then
   # Check if there are any other changed files
   if [ "$(git diff --name-only HEAD^ HEAD | wc -l)" -eq 1 ]; then
-    echo "🛑 - Build cancelled (only todo.txt changed)"
+    echo "🛑 - Build cancelled (only todoList.txt changed)"
     exit 0
   fi
 fi
