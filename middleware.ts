28b1060b1badb1ec0e356de4edb01ea4193e93c8
romankleiner <romank66@yahoo.com>
2026-03-13 06:24:35 +0000

    Whitelist cron job path

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)',
   '/sign-up(.*)',
   '/api/board-image(.*)', 
+  '/api/backup/automated',
 ])
 
 export default clerkMiddleware(async (auth, request) => {
